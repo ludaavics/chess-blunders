3e0ca4ed5f80b65a4780d93a3e7f7e12bdda7d65
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-02-15 01:21:25 -0500

    Clean up dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,36 +3,13 @@ import pathlib
 import random
 
 import pytest
-from fastapi.testclient import TestClient
-from httpx import AsyncClient
-
-from chess_blunders.app.api.main import app as api_app
-
-
-@pytest.fixture(scope="session")
-def api_client():
-    return TestClient(api_app)
-
-
-@pytest.fixture()
-async def async_api_client():
-    async with AsyncClient(app=api_app, base_url="http://test") as client:
-        yield client
-
-
-@pytest.fixture()
-def chessdotcom_username():
-    return "hikaru"
-
-
-@pytest.fixture()
-def chessdotcom_invalid_username():
-    return "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=40))
 
 
 @pytest.fixture()
 def games():
-    with open(pathlib.Path(__file__).parent.absolute() / "fixtures/games.json") as f:
+    with open(
+        pathlib.Path(__file__).parent.absolute() / "fixtures/core/games.json"
+    ) as f:
         return json.load(f)
 
 
@@ -62,8 +39,8 @@ def get_games_chessdotcom_invalid_username_event(get_games_chessdotcom_event):
     )
 
 
-@pytest.fixture(params=[(slice(1, 3), ["white", "black"])])
-def post_blunders_event(request):
+@pytest.fixture()
+def post_blunders_event():
     return event("post_blunders")
 
 
--- tests/snapshots/snap_test_api.py
+++ tests/snapshots/snap_test_api.py
@@ -6,6 +6,18 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
+snapshots["test_post_blunders 1"] = {
+    "body": """[
+  {
+    "starting_fen": "r5k1/6p1/2p3Q1/1pqnp2p/8/1BPR4/1PP2PPP/6K1 b - - 5 31",
+    "pgn": "30... Kg8 { [%clk 0:01:21.2] } 31. Rd3 { [%clk 0:00:03.3] } ( { Refutation (-99998) } 31. Rd3 Ra1+ 32. Rd1 Rxd1# ) ( { Solution (646) } 31. h3 Rf8 ) 31... Ra1+ { [%clk 0:01:21.4] }",
+    "cp_loss": -100644.0,
+    "probability_loss": -0.9298247211092094
+  }
+]""",
+    "statusCode": 200,
+}
+
 snapshots["test_post_blunders[post_blunders_event0] 1"] = {
     "body": """[
   {
