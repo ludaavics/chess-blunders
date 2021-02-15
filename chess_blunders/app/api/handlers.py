8fac8e59cb92c048a8a3f91d8c12fe25bb0a3f28
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-02-14 20:31:44 -0500

    Switch to async handlers

--- chess_blunders/app/api/handlers.py
+++ chess_blunders/app/api/handlers.py
@@ -1,22 +1,38 @@
+import asyncio
 import json
 
+import wrapt
 
-def _jsonify(status_code, data, message):
+
+# ------------------------------------------------------------------------------------ #
+#                                        Helpers                                       #
+# ------------------------------------------------------------------------------------ #
+def jsonify(status_code, data, message):
     body = {"data": data, "message": message}
     return {"statusCode": status_code, "body": json.dumps(body, indent=2)}
 
 
-def get_games_chessdotcom(event, context):
+@wrapt.decorator
+def async_handler(handler, instance, args, kwargs):
+    return asyncio.run(handler(*args, **kwargs))
+
+
+# ------------------------------------------------------------------------------------ #
+#                                       Handlers                                       #
+# ------------------------------------------------------------------------------------ #
+@async_handler
+async def get_games_chessdotcom(event, context):
     """
     Get all the games from a chess.com user.
     """
     username = event["pathParameters"]["username"]
-    return _jsonify(200, "", f"Get chess.com games from {username}")
+    return jsonify(200, "", f"Get chess.com games from {username}")
 
 
-def blunders(event, context):
+@async_handler
+async def blunders(event, context):
     """
     Create puzzles from blunders in a list of games.
     """
     data = json.loads(event.get("body", "{}"))
-    return _jsonify(200, data, "Create puzzles from data.")
+    return jsonify(200, data, "Create puzzles from data.")
