a9dc6da3f260dd19e581131b366a7eb4bcf57982
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-02-19 22:26:15 -0500

    Test result queue in core blunders

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,13 +1,16 @@
+import asyncio
+
 import pytest
 
 import chess_blunders
 
 
-@pytest.mark.parametrize("games_loc", [0, slice(1, 3)])
+@pytest.mark.parametrize("games_loc,use_queue", [(0, False), (slice(1, 3), True)])
 @pytest.mark.asyncio
-async def test_known_blunders(games, games_loc, snapshot):
+async def test_known_blunders(games, games_loc, use_queue, snapshot):
+    queue = asyncio.Queue() if use_queue else None
     blunders = await chess_blunders.blunders(
-        games[games_loc], max_variation_plies=1, n_engines=-2
+        games[games_loc], max_variation_plies=1, n_engines=-2, results=queue
     )
     snapshot.assert_match([blunder.dict() for blunder in blunders])
 
