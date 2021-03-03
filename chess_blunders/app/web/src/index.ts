0d22ff8a1ab5b9fc58ddc083f41692c9ef9ac5c6
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-03-03 15:04:31 -0500

    Print out retrieved blunders

--- chess_blunders/app/web/src/index.ts
+++ chess_blunders/app/web/src/index.ts
@@ -1,6 +1,30 @@
 import { Chessground } from 'chessground';
 import './styles/chessground.css';
 import './styles/chessground-theme.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const config = {};
-Chessground(document.getElementById('chessground'), config);
+Chessground(document.getElementById('puzzle'), config);
+
+const socket = new WebSocket('wss://so6khh47cg.execute-api.us-east-1.amazonaws.com/dev');
+
+document.forms['blunders-form'].onsubmit = function () {
+  const outgoingMessage = {
+    action: 'request-blunders',
+    n_games: 3,
+    username: this.username.value,
+    source: this.source.value,
+  };
+  console.log(outgoingMessage);
+  socket.send(JSON.stringify(outgoingMessage));
+  return false;
+};
+
+socket.onmessage = function (event) {
+  const message = JSON.parse(event.data);
+  console.log(message);
+  if (message.action === 'blunder') {
+    const blunderText = `${message.blunder.pgn}\n\n`;
+    document.getElementById('blunders').textContent += blunderText;
+  }
+};
