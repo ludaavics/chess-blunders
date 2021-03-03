7b457372fee46c1e20cefb8d86940679a58b0779
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-03-03 15:03:25 -0500

    Empty chess board

---
+++ chess_blunders/app/web/src/index.ts
@@ -0,0 +1,6 @@
+import { Chessground } from 'chessground';
+import './styles/chessground.css';
+import './styles/chessground-theme.css';
+
+const config = {};
+Chessground(document.getElementById('chessground'), config);
