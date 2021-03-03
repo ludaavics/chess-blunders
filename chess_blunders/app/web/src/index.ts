6559735bbc4fdff7eeca51add68b0367d07c4c75
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-03-03 15:13:53 -0500

    Store blunders in session storage

--- chess_blunders/app/web/src/index.ts
+++ chess_blunders/app/web/src/index.ts
@@ -8,23 +8,31 @@ Chessground(document.getElementById('puzzle'), config);
 
 const socket = new WebSocket('wss://so6khh47cg.execute-api.us-east-1.amazonaws.com/dev');
 
-document.forms['blunders-form'].onsubmit = function () {
+function requestBlunders(
+  username: string, source: string, nGames: number = 3, nodes: number = 500000,
+) {
   const outgoingMessage = {
     action: 'request-blunders',
-    n_games: 3,
-    username: this.username.value,
-    source: this.source.value,
+    n_games: nGames,
+    username,
+    source,
+    nodes,
   };
   console.log(outgoingMessage);
   socket.send(JSON.stringify(outgoingMessage));
+}
+document.forms['blunders-form'].onsubmit = function () {
+  requestBlunders(this.username.value, this.source.value);
   return false;
 };
 
-socket.onmessage = function (event) {
+socket.onmessage = (event) => {
+  const nextBlunders: Array<any> = JSON.parse(window.sessionStorage.getItem('nextBlunders')) ?? [];
+
   const message = JSON.parse(event.data);
-  console.log(message);
   if (message.action === 'blunder') {
-    const blunderText = `${message.blunder.pgn}\n\n`;
-    document.getElementById('blunders').textContent += blunderText;
+    nextBlunders.push(message.blunder);
+    window.sessionStorage.setItem('nextBlunders', JSON.stringify(nextBlunders));
+    console.log(`We now have ${nextBlunders.length} blunders in store..`);
   }
 };
